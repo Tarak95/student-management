8d24941698e605972f36cb549133d34181ff2c94
Tarak <tarek8295@gmail.com>
2026-04-23 10:06:52 +0600

    New update added

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 let dbConnection = () => {
     mongoose.connect(process.env.DB_URL)
-    .then(() => console.log("✅ Connected to MongoDB"))
-    .catch((err) => console.log("❌ DB Error:", err));
+    .then(() => console.log(" Connected to MongoDB"))
+    .catch((err) => console.log(" DB Error:", err));
 }
 module.exports = dbConnection;
